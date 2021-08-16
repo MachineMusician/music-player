d6946f04316665bdb5d3046521ab09fbc5c81694
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-17 00:27:35 +0900

    Made a navbar layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,27 @@
+import { FaVolumeMute } from "react-icons/fa";
+
 const Navbar = () => {
-  return <div className="">Navbar is here</div>;
+  return (
+    <header className="navbar">
+      <img className="navbar__logo" src="logo.png" alt="logo" />
+
+      <div className="navbar__controlPart">
+        <input
+          className="navbar__controlPart__input"
+          type="text"
+          placeholder="search here!!"
+        />
+        <FaVolumeMute className="navbar__controlPart__volume" />
+      </div>
+
+      <nav className="navbar__nav">
+        <ul className="nav__list">
+          <li className="nav__list__item">DashBoard</li>
+          <li className="nav__list__item">Go to Play</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
