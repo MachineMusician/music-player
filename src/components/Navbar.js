31fbb415710d2e3e7af27ed3b830da86fd2f5943
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-18 00:12:11 +0900

    Can show nav-item when it's clicked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,46 +11,6 @@ const Navbar = () => {
   };
 
   return (
-    /*
-    <div className="navbar-column">
-          <img
-            className="navbar__logo"
-            src="logo.png"
-            alt="logo"
-            onClick={() => {
-              setMenuClicked(false);
-            }}
-          />
-        </div>
-
-        <div
-          className={
-            menuClicked
-              ? "navbar-column desktop-only deActive"
-              : "navbar-column desktop-only"
-          }
-        >
-          <div className="navbar__controlPart">
-            <input
-              className="navbar__controlPart__input"
-              type="text"
-              placeholder="search here!!"
-            />
-            <FaVolumeMute className="navbar__controlPart__volume" />
-          </div>
-
-          <nav className="navbar__nav">
-            <ul className="nav__list">
-              <li className="nav__list__item">DashBoard</li>
-              <li className="nav__list__item">Go to Play</li>
-            </ul>
-          </nav>
-        </div>
-
-        <div className="navbar-column navbar__controls" onClick={toggleMenu}>
-          {menuClicked ? <AiOutlineClose /> : <GiHamburgerMenu />}
-        </div> */
-
     <header className="navbar">
       <div className="navbar-container container">
         <img
@@ -61,11 +21,8 @@ const Navbar = () => {
             setMenuClicked(false);
           }}
         />
-        <div className="navbar__menu-icon" onClick={toggleMenu}>
-          {menuClicked ? <AiOutlineClose /> : <GiHamburgerMenu />}
-        </div>
 
-        <nav className="navbar__nav">
+        <nav className={menuClicked ? "navbar__nav active" : "navbar__nav"}>
           <ul className="nav__list">
             <li className="nav__list__item">
               <input
@@ -79,6 +36,9 @@ const Navbar = () => {
             <li className="nav__list__item">Go to Play</li>
           </ul>
         </nav>
+        <div className="navbar__menu-icon" onClick={toggleMenu}>
+          {menuClicked ? <AiOutlineClose /> : <GiHamburgerMenu />}
+        </div>
       </div>
     </header>
   );
