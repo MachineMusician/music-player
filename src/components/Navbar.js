9e73b4430b668cf2adeb182f3d065745286e5e68
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-17 22:38:42 +0900

    Added icons for mobile screen, and made var.scss

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,42 @@
+import { useState } from "react";
 import { FaVolumeMute } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [menuClicked, setMenuClicked] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuClicked(!menuClicked);
+  };
+
   return (
     <header className="navbar">
-      <img className="navbar__logo" src="logo.png" alt="logo" />
-
-      <div className="navbar__controlPart">
-        <input
-          className="navbar__controlPart__input"
-          type="text"
-          placeholder="search here!!"
-        />
-        <FaVolumeMute className="navbar__controlPart__volume" />
+      <div className="navbar-column">
+        <img className="navbar__logo" src="logo.png" alt="logo" />
       </div>
 
-      <nav className="navbar__nav">
-        <ul className="nav__list">
-          <li className="nav__list__item">DashBoard</li>
-          <li className="nav__list__item">Go to Play</li>
-        </ul>
-      </nav>
+      <div className="navbar-column desktop-only">
+        <div className="navbar__controlPart">
+          <input
+            className="navbar__controlPart__input"
+            type="text"
+            placeholder="search here!!"
+          />
+          <FaVolumeMute className="navbar__controlPart__volume" />
+        </div>
+
+        <nav className="navbar__nav">
+          <ul className="nav__list">
+            <li className="nav__list__item">DashBoard</li>
+            <li className="nav__list__item">Go to Play</li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="navbar-column navbar__controls" onClick={toggleMenu}>
+        {menuClicked ? <GiHamburgerMenu /> : <AiOutlineClose />}
+      </div>
     </header>
   );
 };
