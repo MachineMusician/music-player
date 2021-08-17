fb5ad27e3c6b44138baa7a5dfd43a2f66cb16613
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-17 23:59:37 +0900

    Renewaled html structure with little bit of css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,31 +11,74 @@ const Navbar = () => {
   };
 
   return (
-    <header className="navbar">
-      <div className="navbar-column">
-        <img className="navbar__logo" src="logo.png" alt="logo" />
-      </div>
-
-      <div className="navbar-column desktop-only">
-        <div className="navbar__controlPart">
-          <input
-            className="navbar__controlPart__input"
-            type="text"
-            placeholder="search here!!"
+    /*
+    <div className="navbar-column">
+          <img
+            className="navbar__logo"
+            src="logo.png"
+            alt="logo"
+            onClick={() => {
+              setMenuClicked(false);
+            }}
           />
-          <FaVolumeMute className="navbar__controlPart__volume" />
+        </div>
+
+        <div
+          className={
+            menuClicked
+              ? "navbar-column desktop-only deActive"
+              : "navbar-column desktop-only"
+          }
+        >
+          <div className="navbar__controlPart">
+            <input
+              className="navbar__controlPart__input"
+              type="text"
+              placeholder="search here!!"
+            />
+            <FaVolumeMute className="navbar__controlPart__volume" />
+          </div>
+
+          <nav className="navbar__nav">
+            <ul className="nav__list">
+              <li className="nav__list__item">DashBoard</li>
+              <li className="nav__list__item">Go to Play</li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className="navbar-column navbar__controls" onClick={toggleMenu}>
+          {menuClicked ? <AiOutlineClose /> : <GiHamburgerMenu />}
+        </div> */
+
+    <header className="navbar">
+      <div className="navbar-container container">
+        <img
+          className="navbar__logo"
+          src="logo.png"
+          alt="logo"
+          onClick={() => {
+            setMenuClicked(false);
+          }}
+        />
+        <div className="navbar__menu-icon" onClick={toggleMenu}>
+          {menuClicked ? <AiOutlineClose /> : <GiHamburgerMenu />}
         </div>
 
         <nav className="navbar__nav">
           <ul className="nav__list">
+            <li className="nav__list__item">
+              <input
+                className="list__item__input"
+                type="text"
+                placeholder="search here!!"
+              />
+              <FaVolumeMute className="list__item__volume" />
+            </li>
             <li className="nav__list__item">DashBoard</li>
             <li className="nav__list__item">Go to Play</li>
           </ul>
         </nav>
-      </div>
-
-      <div className="navbar-column navbar__controls" onClick={toggleMenu}>
-        {menuClicked ? <GiHamburgerMenu /> : <AiOutlineClose />}
       </div>
     </header>
   );
