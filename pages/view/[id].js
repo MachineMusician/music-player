561f3a542864b37da8ee492707eca7b5ed958e5a
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-19 16:42:17 +0900

    Fixed CORS error. It was an error just for herokuapp

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,12 +1,13 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 const View = () => {
   const router = useRouter();
   const { id } = router.query;
   const [data, setData] = useState([]);
+  //   const API_URL = `https://cors-anywhere.herokuapp.com/http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
   const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
 
   const regex_date = /[0-9]+-[0-9]+-[0-9]+/;
