eeb7d5ec1ed58045958eee94d222ea4d6d5c9822
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-19 18:44:46 +0900

    Could get detail data when I clicked the item, but couldn't get detail when it's refreshed

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -6,17 +6,15 @@ import axios from "axios";
 const View = () => {
   const router = useRouter();
   const { id } = router.query;
-  const [data, setData] = useState([]);
-  //   const API_URL = `https://cors-anywhere.herokuapp.com/http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const [data, setData] = useState({});
   const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  // const API_URL = `https://cors-anywhere.herokuapp.com/http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  // const API_URL = `Access-Control-Allow-Origin:http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
 
   const regex_date = /[0-9]+-[0-9]+-[0-9]+/;
 
   const getData = () => {
-    axios
-      .get(API_URL) //
-      .then((res) => setData(res.data))
-      .catch(console.log);
+    axios.get(API_URL).then((res) => setData(res.data));
   };
 
   useEffect(() => {
@@ -26,36 +24,37 @@ const View = () => {
   return (
     <>
       <Head>
-        <title>Music Player</title>
-        <meta
-          name="description"
-          content="It's a website for playing music, using AI"
-        />
+        <title>Details</title>
+        <meta name="description" content="playing music" />
       </Head>
       <div className="view-container">
-        <div className="view__info">
-          {console.log(data)}
-          <div key={data.id} className="view__info"></div>
-          <img
-            className="view__info__item__image"
-            src={data.image_link}
-            alt="product image"
-          />
-          <div className="view__info__item__info">
-            <span className="view__info__item__info__name">
-              {/* {data.name.trim().length < 42
-              ? data.name
-              : `${data.name.slice(0, 40)}...`} */}
-            </span>
-            <span className="view__info__item__info__product-type">
-              {data.product_type}
-            </span>
-            <span className="view__info__item__info__createdAt">
-              {/* {data.created_at.match(regex_date)} */}
-            </span>
+        <div className="view__column info-section">
+          <div key={data.id} className="info-section__item">
+            <img
+              className="info-section__item__image"
+              src={data.image_link}
+              alt="product image"
+            />
+            <div className="info-section__item__info">
+              <span className="info-section__item__info__name">
+                {/* {data.name.trim().length < 42
+                  ? data.name
+                  : `${data.name.slice(0, 40)}...`} */}
+                {data.name}
+              </span>
+              <span className="info-section__item__info__product-type">
+                {data.product_type}
+              </span>
+              <span className="info-section__item__info__createdAt">
+                {/* {data.created_at.match(regex_date)} */}
+              </span>
+              <p className="info-section__item__info__description">
+                {data.description}
+              </p>
+            </div>
           </div>
         </div>
-        <div className="view__play"></div>
+        <div className="view__column play-section">hello</div>
       </div>
     </>
   );
