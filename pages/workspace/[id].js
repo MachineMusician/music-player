3b4ce079f0c0e6e8f9fee6107d07251380464c11
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-20 15:55:51 +0900

    Done with the layout on go-to-play page roughly

--- pages/workspace/[id].js
+++ pages/workspace/[id].js
@@ -1,12 +1,14 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Head from "next/head";
 import { GoPlus } from "react-icons/go";
-import { useState } from "react";
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 
 const Workspace = () => {
   const router = useRouter();
   const { id } = router.query; // info of random user
   const [fileString, setFileString] = useState("");
+  const [playMusic, setPlayMusic] = useState(false);
 
   const onFileChange = (event) => {
     const {
@@ -16,13 +18,23 @@ const Workspace = () => {
 
     const reader = new FileReader();
     reader.onloadend = (finishedEvent) => {
+      // convert image file to base64 string
       const {
         currentTarget: { result },
       } = finishedEvent;
       setFileString(result);
     };
-    reader.readAsDataURL(targetFile);
+    if (targetFile) {
+      reader.readAsDataURL(targetFile);
+    }
   };
+
+  const handlePlay = () => {
+    setPlayMusic(!playMusic);
+  };
+
+  const handleDuration = () => {};
+
   return (
     <>
       <Head>
@@ -30,7 +42,38 @@ const Workspace = () => {
         <meta name="description" content="playing music" />
       </Head>
       <div className="workspace">
-        <form>
+        {fileString && ( //
+          <div className="workspace__work">
+            <img
+              className="workspace__work__img"
+              src={fileString}
+              width="100px"
+              height="100px"
+            />
+            <div className="workspace__work__controller">
+              {playMusic ? (
+                <AiFillPauseCircle
+                  className="workspace__work__controller__icon"
+                  onClick={handlePlay}
+                />
+              ) : (
+                <AiFillPlayCircle
+                  className="workspace__work__controller__icon"
+                  onClick={handlePlay}
+                />
+              )}
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value="0"
+                id="workspace__work__controller__slidebar"
+                onChange={handleDuration}
+              />
+            </div>
+          </div>
+        )}
+        <form id="workspace__form">
           <label htmlFor="upload-image">
             <div className="workspace__uploadImg">
               <GoPlus className="workspace__uploadImg__icon" />
@@ -46,8 +89,6 @@ const Workspace = () => {
             onChange={onFileChange}
             style={{ visibility: "hidden" }}
           />
-
-          {fileString && <img src={fileString} width="100px" height="100px" />}
         </form>
       </div>
     </>
