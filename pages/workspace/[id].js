4079c0ace97e39102f76ce5b7ec63a226d91cf1d
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-20 09:35:32 +0900

    About to make upload image, and I used label to show better than original input tag

--- pages/workspace/[id].js
+++ pages/workspace/[id].js
@@ -1,9 +1,33 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 const Workspace = () => {
   const router = useRouter();
   const { id } = router.query;
-  return <div className="workspace">{console.log(id)}Workspace here</div>;
+  return (
+    <>
+      <Head>
+        <title>Go To Play</title>
+        <meta name="description" content="playing music" />
+      </Head>
+      <div className="workspace">
+        <form>
+          <label htmlFor="upload-image">
+            <span>Upload Image!</span>
+          </label>
+
+          <input
+            id="upload-image"
+            className=""
+            type="file"
+            accept="image/*"
+            // onChange={onFileChange}
+            style={{ visibility: "hidden" }}
+          />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Workspace;
