95594a5fceb9f60b517fe938a204e90236870a2b
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-25 22:29:32 +0900

    Can scroll horizontally using mouse only

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,27 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
-import scrollr from "../public/skrollr";
 
 export default function Home() {
   const [position, setPosition] = useState(0);
+  const SCROLL_SPEED = 5;
   // const scrollContainer = document.querySelector("welcome");
 
   const onScroll = () => {
     setPosition(window.scrollY);
   };
 
-  // const onWheel = (event) => {
-  //   event.preventDefault();
-  //   console.log(event.currentTarget.scrollLeft, event.deltaX);
-  //   event.currentTarget.scrollLeft += event.deltaY;
-  // };
+  const onWheel = (event) => {
+    event.preventDefault();
+    event.currentTarget.scrollLeft += event.deltaY / SCROLL_SPEED;
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", onScroll);
 
-    // scrollContainer.addEventListener("wheel", onWheel);
-
     return () => {
       window.removeEventListener("scroll", onScroll);
-      // scrollContainer.removeEventListener("wheel", onWheel);
     };
   }, []);
-
-  // const child = {
-  //   width: "300em",
-  //   height: "100%",
-  //   position: "absolute",
-  //   inset: "0",
-  // };
 
   return (
     <>
@@ -44,11 +33,7 @@ export default function Home() {
         />
       </Head>
       <div className="welcome">
-        <div
-          className="welcome__container"
-          // data-0="transform:translateX(0%);"
-          // data-1000="transform:translateX(-100%);"
-        >
+        <div className="welcome__container" onWheel={onWheel}>
           <section>
             <div className="content">
               <h2>section 1</h2>
