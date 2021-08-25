762c89d83e335d25838b999518a5c297052b87ef
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-25 23:18:37 +0900

    Removed scroll event and used wheel event for scrolling screen horizontally and parallex scrolling

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,13 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [position, setPosition] = useState(0);
-  const SCROLL_SPEED = 5;
-  // const scrollContainer = document.querySelector("welcome");
-
-  const onScroll = () => {
-    setPosition(window.scrollY);
-  };
+  const SCROLL_SPEED = 1.5;
 
   const onWheel = (event) => {
     event.preventDefault();
     event.currentTarget.scrollLeft += event.deltaY / SCROLL_SPEED;
+    setPosition(event.currentTarget.scrollLeft);
   };
-
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, []);
 
   return (
     <>
@@ -35,41 +23,21 @@ export default function Home() {
       <div className="welcome">
         <div className="welcome__container" onWheel={onWheel}>
           <section>
-            <div className="content">
-              <h2>section 1</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
-                nostrum sed minima ipsum? Excepturi accusamus ipsa illo,
-                cupiditate libero quod voluptates similique laborum dolor
-                assumenda asperiores quisquam, hic quasi fuga.
-              </p>
+            <div
+              className="welcome__container__content ani1"
+              style={{ transform: `translateX(${position}px)` }}
+            >
+              <h2>Take a picture of your note!!</h2>
+              <p>You can play your note before you play it.</p>
+            </div>
+            <div className="welcome__container__content ani2">
+              <img src="./note2.jpg" alt="hi" />
             </div>
           </section>
+
           <section>
-            <div className="content">
+            <div className="welcome__container__content">
               <h2>section 2</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
-                nostrum sed minima ipsum? Excepturi accusamus ipsa illo,
-                cupiditate libero quod voluptates similique laborum dolor
-                assumenda asperiores quisquam, hic quasi fuga.
-              </p>
-            </div>
-          </section>
-          <section>
-            <div className="content">
-              <h2>section 3</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
-                nostrum sed minima ipsum? Excepturi accusamus ipsa illo,
-                cupiditate libero quod voluptates similique laborum dolor
-                assumenda asperiores quisquam, hic quasi fuga.
-              </p>
-            </div>
-          </section>
-          <section>
-            <div className="content">
-              <h2>section 4</h2>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
                 nostrum sed minima ipsum? Excepturi accusamus ipsa illo,
