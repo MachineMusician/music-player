79934d529a2adf6c9825430af489afe8e6a3fd0a
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-09-08 22:31:42 +0900

    Added music controller

--- next.config.js
+++ next.config.js
@@ -1,3 +1,34 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  // webpack: (config, { dev }) => {
+  //   config.module.rules.push(
+  //     {
+  //       test: /\.css$/,
+  //       loader: "style-loader!css-loader",
+  //     },
+  //     {
+  //       test: /\.s[a|c]ss$/,
+  //       loader: "sass-loader!style-loader!css-loader",
+  //     },
+  //     {
+  //       test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+  //       use: {
+  //         loader: "url-loader",
+  //         options: {
+  //           limit: 100000,
+  //           publicPath: "./",
+  //           outputPath: "static/",
+  //           name: "[name].[ext]",
+  //         },
+  //       },
+  //     },
+  //     {
+  //       test: [/\.eot$/, /\.ttf$/, /\.svg$/, /\.woff$/, /\.woff2$/],
+  //       loader: require.resolve("file-loader"),
+  //       options: {
+  //         name: "/static/media/[name].[hash:8].[ext]",
+  //       },
+  //     }
+  //   );
+  //   return config;
+  // },
+};
--- pages/workspace/[id].js
+++ pages/workspace/[id].js
@@ -1,5 +1,5 @@
 // import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import Head from "next/head";
 import { GoPlus } from "react-icons/go";
@@ -11,8 +11,11 @@ import {
 } from "react-icons/ai";
 import Modal from "../../src/components/Modal";
 import Workspace_showHow from "../../src/components/Workspace_ShowHow";
+import Slider from "../../src/components/Slider";
+import ControlPanel from "../../src/components/ControlPanel";
 import "cropperjs/dist/cropper.css";
 import Workspace_Cropper from "../../src/components/Workspace_Cropper";
+// import song from "../../public/test2.mp3";
 
 const Workspace = () => {
   // const router = useRouter();
@@ -22,12 +25,52 @@ const Workspace = () => {
   const [clickInfo, setClickInfo] = useState(false);
   const [cropData, setCropData] = useState("");
   const [openCropperModal, setOpenCropperModal] = useState(false);
-
+  //
   const [waveFile, setWaveFile] = useState([]);
   const [imageFile, setImageFile] = useState([]);
   const [contents, setContents] = useState(null);
-
   const API_URL = "http://127.0.0.1:8000/add_music";
+  //
+
+  const [percentage, setPercentage] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const audioRef = useRef();
+
+  const onChange = (e) => {
+    const audio = audioRef.current;
+    audio.currentTime = (audio.duration / 100) * e.target.value;
+    setPercentage(e.target.value);
+  };
+
+  const play = () => {
+    const audio = audioRef.current;
+    audio.volume = 0.1;
+
+    if (!isPlaying) {
+      setIsPlaying(true);
+      audio.play();
+    }
+
+    if (isPlaying) {
+      setIsPlaying(false);
+      audio.pause();
+    }
+  };
+
+  const getCurrDuration = (e) => {
+    const percent = (
+      (e.currentTarget.currentTime / e.currentTarget.duration) *
+      100
+    ).toFixed(2);
+    const time = e.currentTarget.currentTime;
+
+    setPercentage(+percent);
+    setCurrentTime(time.toFixed(2));
+  };
+
   const sendImage = async () => {
     try {
       await axios.post(API_URL, {
@@ -111,7 +154,7 @@ const Workspace = () => {
                       />
                     )}
                     <div className="workspace__work-list__controller">
-                      {playMusic ? (
+                      {/* {playMusic ? (
                         <AiFillPauseCircle
                           className="workspace__work-list__controller__icon"
                           onClick={handlePlay}
@@ -128,6 +171,21 @@ const Workspace = () => {
                         max="100"
                         value="0"
                         id="workspace__work-list__controller__slidebar"
+                      /> */}
+                      <Slider percentage={percentage} onChange={onChange} />
+                      <audio
+                        ref={audioRef}
+                        onTimeUpdate={getCurrDuration}
+                        onLoadedData={(e) => {
+                          setDuration(e.currentTarget.duration.toFixed(2));
+                        }}
+                        src={"../../public/test2.mp3"}
+                      ></audio>
+                      <ControlPanel
+                        play={play}
+                        isPlaying={isPlaying}
+                        duration={duration}
+                        currentTime={currentTime}
                       />
                     </div>
                   </li>
